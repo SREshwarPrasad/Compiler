2c3f0feaf92d0cda9b79c3c3080428cbe7f71da0
S R Eshwar Prasad <141043413+SREshwarPrasad@users.noreply.github.com>
2025-02-22 22:27:11 +0530

    Update if.c

--- if.c
+++ if.c
@@ -26,3 +26,106 @@ else {return ELSE;}
 [ \t] {}
 . {;}
 %%
+
+
+YACC:
+%{
+	#include<stdio.h>
+	#include<stdlib.h>
+	int yylex();
+	int yyerror(char *);
+%}
+
+%token IF ELSE id nums plus minus LT GT scolon equal NL
+%token lp rp lc rc
+
+%%
+
+S: W{printf("\nInput accepted\n"); exit(0);};
+W: 			   IF lp cond rp NL lc NL W NL rc NL 											//if 
+				| IF lp cond rp NL lc NL W NL rc NL ELSE NL lc NL W NL rc 		//if else
+				| IF lp cond rp NL lc NL W NL rc G ELSE NL lc NL W NL rc   		//if else if
+				|E; 
+
+G: 			NL ELSE IF lp cond rp NL lc NL W NL rc NL 
+				| NL ELSE IF lp cond rp NL lc NL W NL rc NL G;
+
+cond: 		id op1 id
+				|id op1 nums;
+
+E: 			id equal K scolon;
+
+K: 			val op2 K
+				|val;
+
+val:			id
+				|nums;
+				
+op1:			LT
+				|GT;
+
+op2:			plus
+				|minus;
+%%
+
+int main() { while(1) {yyparse();} return 0; }
+int yywrap() {return 0;}
+int yyerror(char *s){printf("error... %s\n",s); exit(0); return 0;}
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+/*
+IF lp cond rp  
+lc  
+	IF lp cond rp  
+	lc  
+		W  
+	rc  
+	ELSE IF lp cond rp  
+	lc 
+		 W  
+	rc  
+	 
+	ELSE IF lp cond rp  
+	lc  
+		W  
+	rc  
+	 
+	ELSE IF lp cond rp 
+	lc  
+		W  
+	rc  
+	 
+	ELSE IF lp cond rp 
+	lc  
+		W  
+	rc  
+	ELSE  
+	lc  
+		W 
+	rc 
+	 
+rc  
+
+....
+
+*/
+
+
+
+
